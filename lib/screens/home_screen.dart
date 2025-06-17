1ac9080089ca89d7f2c022919e8e5a8ddb4554c3
AlejoMarti <alejomarti95@gmail.com>
2025-06-17 16:44:43 -0500

    card images added + floating button (+) added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,33 +5,64 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 125,
-          child: Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  height: 125,
-                  width: 100,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Container(),
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          _RecipesCard(context),
+          _RecipesCard(context),
+          _RecipesCard(context),
+          _RecipesCard(context),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
+        child: Icon(Icons.add, color: Colors.white),
+        onPressed: (){}),
+
+    );
+  }
+
+  
+
+  Widget _RecipesCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: 125,
+        child: Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 125,
+                width: 100,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network('https://static.platzi.com/media/uploads/flutter_lasana_b894f1aee1.jpg', fit: BoxFit.cover),
+                ),
+              ),
+              SizedBox(width: 26),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Lasagna',
+                    style: TextStyle(fontSize: 16, fontFamily: "Quicksand"),
                   ),
-                ),
-                SizedBox(width: 26),
-                Column(
-                  children: <Widget>[
-                    Text('Lasagna'),
-                    Text("Alejandro M."),
-                    Container(height: 1, width: 50, color: Colors.orange),
-                  ],
-                ),
-              ],
-            ),
+                  SizedBox(height: 4),
+                  Container(height: 1, width: 50, color: Colors.orange),
+                  Text(
+                    "Alejandro M.",
+                    style: TextStyle(fontSize: 16, fontFamily: "Quicksand"),
+                  ),
+                  SizedBox(height: 4),
+                ],
+              ),
+            ],
           ),
         ),
+      ),
     );
   }
 }
